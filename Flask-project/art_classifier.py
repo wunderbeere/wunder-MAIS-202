c5645a0b0cc9051618f6fbb754fb15256a3712b2
Yuliya Shpunarska <yuliya2001@gmail.com>
2020-12-03 17:29:27 -0500

    Production webapp

--- Flask-project/art_classifier.py
+++ Flask-project/art_classifier.py
@@ -8,8 +8,7 @@ import json
 from keras.models import load_model
 from tensorflow.compat.v1.keras.backend import set_session
 from tensorflow.compat.v1 import Session, get_default_graph
-import matplotlib.pyplot as plt
-import tensorflow as tf
+
 import numpy as np
 from keras.preprocessing.image import load_img, img_to_array
 
@@ -31,9 +30,6 @@ graph = get_default_graph()
 global names_top_artists
 names_top_artists = load('names_top_artists.joblib')
 
-#colors = []
-#color_codes = dict(zip(names_top_artists, colors))
-
 @app.route('/', methods=['GET', 'POST'])
 def main_page():
     if request.method == 'POST':
@@ -51,16 +47,15 @@ def predict_result(filename):
     html_path = "/static/" + filename.replace("\\", "")
     image = load_img(path, target_size=(224, 224)) # returns a PIL image
     image = img_to_array(image) # turn into array
-          # reshape into correct format for the model (samples x H x W x channels)
+    # reshape into correct format for the model (samples x H x W x channels)
     image = image.reshape((1, image.shape[0], image.shape[1], image.shape[2]))
 
-          # extract features
+    # extract features
     features = base_model.predict(image)
-          # predict the artist
+    # predict the artist
     yhat = classifier.predict(features.reshape((1, 7*7*512)))
     prediction = names_top_artists[int(yhat)].replace("_", " ") # name of the artist
 
-          ### FIX PATH  Could not build url for endpoint 'uploads\\Vincent_van_Gogh_10.jpg'
     return render_template('predictions.html', prediction=prediction, image_path=html_path)
 
 @app.context_processor
@@ -69,4 +64,5 @@ def artists_info():
     return dict(artists_info_dict=artists_info_dict)
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=80)
+    from waitress import serve
+    serve(app, host="127.0.0.1", port=5000)
--- README.md
+++ README.md
@@ -5,3 +5,26 @@ Currently includes:
 - A Jupyter notebook with the development of the model
 - Saved base model (feature extractor) and classifier
 - Webapp in development
+- Pictures to test the model and screenshots of the final webapp
+
+Try it yourself with venv
+
+On Windows with venv:
+Make the virtual environment
+`mkdir art_classifier
+
+python -m venv art_classifier
+
+art_classifier\Scripts\activate
+
+cd art_classifier`
+
+Install the webapp and run it
+
+`git clone https://github.com/wunderbeere/wunder-MAIS-202
+
+pip install -r requirements.txt
+
+cd Flask-project
+
+python art_classifier.py `
